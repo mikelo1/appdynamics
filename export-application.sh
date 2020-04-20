2250d356336bae4801ad37011cb91b8e89da6e6b
Mikel Osinalde <mosinalde@atsistemas.com>
2020-04-20 13:22:06 +0200

    modules with classes

---
+++ export-application.sh
@@ -0,0 +1,81 @@
+#/bin/bash
+
+###########################################
+### Read credentials from YAML file     ###
+###########################################
+### Script will read from a YAML file   ###
+### with the following format:          ###
+### ----------------------------------- ###
+### <environment>:                      ###
+###     secret:                         ###
+###         username: <user>@<account>  ###
+###         password: <password>        ###
+###         apiclient: <apiClient_name> ###
+###         apisecret: <client_secret>  ###
+###         url: <base_controller_url>  ###
+###########################################
+
+if [ $# -ne 3 ]; then 
+    echo "Syntax: $0 <credentials_yaml_file> <environment> <application>"
+	exit
+fi
+
+CRED_FILE=$1
+ENVIRONMENT=$2
+APPLICATION=$3
+
+USER=`grep $ENVIRONMENT -A6 $CRED_FILE | grep username | awk -F"[:@]" '{print $2}' | sed 's/\s//g'`
+ACCOUNT=`grep $ENVIRONMENT -A6 $CRED_FILE | grep username | awk -F"[:@]" '{print $3}' | sed 's/\s//g'`
+PASS=`grep $ENVIRONMENT -A6 $CRED_FILE | grep password | awk -F: '{print $2}' | sed 's/\s//g'`
+APICLIENT=`grep $ENVIRONMENT -A6 $CRED_FILE | grep apiclient | awk -F: '{print $2}' | sed 's/\s//g'`
+APISECRET=`grep $ENVIRONMENT -A6 $CRED_FILE | grep apisecret | awk -F: '{print $2}' | sed 's/\s//g'`
+HOST=`grep $ENVIRONMENT -A6 $CRED_FILE | grep url | awk -F: '{print $3}' | sed 's/\///g'`
+
+App_List=$(curl -s --user "${USER}@${ACCOUNT}:${PASS}" https://${HOST}/controller/rest/applications/)
+if [ $? -ne 0 ]; then
+	echo "Something went wrong with the cURL command. Exiting..."
+	exit
+fi
+App_Info=$(echo "${App_List}" | grep "<name>$APPLICATION</name>" -B1 -A1)
+if [ $? -ne 0 ]; then
+	echo "Application $APPLICATION not found for account $ACCOUNT. Exiting..."
+	exit
+fi
+APP_ID=$(echo "${App_Info}" | grep "id" | awk -F"[<>]" '{print $3}')
+
+if [ ! -d $APPLICATION ]; then mkdir $APPLICATION; fi
+
+
+#REQUEST SNAPSHOTS
+#curl -s --user "${USER}@${ACCOUNT}:${PASS}" "https://$HOST/controller/rest/applications/$APP_ID/request-snapshots?time-range-type=BEFORE_NOW&duration-in-mins=160&maximum-results=5000&output=JSON" -o $APPLICATION/request-snapshots.json
+#curl -s --user "${USER}@${ACCOUNT}:${PASS}" "https://$HOST/controller/rest/applications/$APP_ID/request-snapshots?time-range-type=BEFORE_NOW&duration-in-mins=1440&maximum-results=5000&application-component-ids=${Tier_ID}&output=JSON" -o $APPLICATION/request-snapshots-${Tier_ID}.json
+
+#HEALTH-RULE-VIOLATIONS
+#curl -s --user "${USER}@${ACCOUNT}:${PASS}" "https://$HOST/controller/rest/applications/$APP_ID/problems/healthrule-violations?time-range-type=AFTER_TIME&duration-in-mins=1440&start-time=1572515956000" -o $APPLICATION/healthrule-violations.xml
+
+#sleep 30s
+
+for FILE in healthrules.xml actions.json policies.json; do
+	ENTITY=`echo $FILE | awk -F. '{print $1}'`
+	EXT=`echo $FILE | awk -F. '{print $2}'`
+#	echo "Fetching $FILE..."
+	curl -s --user "${USER}@${ACCOUNT}:${PASS}" https://$HOST/controller/$ENTITY/$APP_ID -o $APPLICATION/$FILE
+	if [ $? -ne 0 ]; then
+		echo "Something went wrong with the cURL command. Exiting..."
+	else
+		echo -e "$ENTITY from $ACCOUNT downloaded to file ${APPLICATION}/${GREEN}${FILE}${NC}"
+	fi
+done
+
+for FILE in transactiondetection-auto.xml transactiondetection-custom.xml; do
+	ENTITY=`echo $FILE | awk -F[.-] '{print $1}'`
+	TYPE=`echo $FILE | awk -F[.-] '{print $2}'`
+	EXT=`echo $FILE | awk -F[.-] '{print $3}'`
+#	echo "Fetching $FILE..."
+	curl -s --user "${USER}@${ACCOUNT}:${PASS}" https://$HOST/controller/$ENTITY/$APP_ID/$TYPE -o $APPLICATION/$FILE
+	if [ $? -ne 0 ]; then
+		echo "Something went wrong with the cURL command. Exiting..."
+	else
+		echo -e "$ENTITY from $ACCOUNT downloaded to file ${APPLICATION}/${GREEN}${FILE}${NC}"
+	fi
+done
--- exportCSV.sh
+++ exportCSV.sh
@@ -39,59 +39,60 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 
 if [ ! -d $APPLICATION ]; then mkdir $APPLICATION; fi
 
+#HEALTH-RULE-VIOLATIONS
+#$SCRIPTPATH/exportCSV.py events -s -P 443 -o $APPLICATION/events.csv -H ${HOST} -u ${USER} -p ${PASS} -a ${APPLICATION}
+
+#DASHBOARDS
+#$SCRIPTPATH/exportCSV.py dashboards -s -P 443 -o $APPLICATION/dashboards.csv -H ${HOST} -u ${USER} -p ${PASS} --api-client-name ${APICLIENT} --api-client-secret ${APISECRET}
+
+#REQUEST SNAPSHOTS
+#$SCRIPTPATH/exportCSV.py snapshots -s -p 443 -o $APPLICATION/request-snapshots.csv -H ${HOST} -u ${USER} -p ${PASS} -a ${APPLICATION}
+
 for FILE in healthrules.xml actions.json policies.json; do
 	ENTITY=`echo $FILE | awk -F. '{print $1}'`
 	EXT=`echo $FILE | awk -F. '{print $2}'`
-#	if [ ! -f $APPLICATION/$FILE ]; then
-#		echo "Fetching $FILE..."
-#		curl -s --user $USER:$PASS https://$HOST/controller/$ENTITY/$APP_ID -o $APPLICATION/$FILE -k
-#	fi
-#	echo "Converting file $FILE to CSV..."
-#	$SCRIPTPATH/exportCSV.py $ENTITY -i $APPLICATION/$FILE -o $APPLICATION/$ENTITY.csv
+#	if [ -f $APPLICATION/$FILE ]; then
+#		echo "Converting file $FILE to CSV..."
+#		$SCRIPTPATH/exportCSV.py $ENTITY -i $APPLICATION/$FILE -o $APPLICATION/$ENTITY.csv
+#	else
 	echo "Fetch data and translating to CSV..."
 	$SCRIPTPATH/exportCSV.py $ENTITY -s -P 443 -o $APPLICATION/$ENTITY.csv -H ${HOST} -u ${USER} -p ${PASS} -a ${APPLICATION}
+#	fi
 done
 
 for FILE in business-transactions.json backends.json; do
 	ENTITY=`echo $FILE | awk -F. '{print $1}'`
 	EXT=`echo $FILE | awk -F. '{print $2}'`
-#	if [ ! -f $APPLICATION/$FILE ]; then
-#		echo "Fetching $FILE..."
-#		curl -s --user $USER:$PASS "https://$HOST/controller/rest/applications/$APP_ID/$ENTITY?output=JSON" -o $APPLICATION/$FILE -k
-#	fi
-#	echo "Converting file $FILE to CSV..."
-#	$SCRIPTPATH/exportCSV.py $ENTITY -i $APPLICATION/$FILE -o $APPLICATION/$ENTITY.csv
+#	if [ -f $APPLICATION/$FILE ]; then
+#		echo "Converting file $FILE to CSV..."
+#		$SCRIPTPATH/exportCSV.py $ENTITY -i $APPLICATION/$FILE -o $APPLICATION/$ENTITY.csv
+#	else
 	echo "Fetch data and translating to CSV..."
 	$SCRIPTPATH/exportCSV.py $ENTITY -s -P 443 -o $APPLICATION/$ENTITY.csv -H ${HOST} -u ${USER} -p ${PASS} -a ${APPLICATION}
+#	fi
 done
 
 for FILE in transactiondetection-auto.xml transactiondetection-custom.xml; do
 	ENTITY=`echo $FILE | awk -F[.-] '{print $1}'`
 	TYPE=`echo $FILE | awk -F[.-] '{print $2}'`
 	EXT=`echo $FILE | awk -F[.-] '{print $3}'`
-#	if [ ! -f $APPLICATION/$FILE ]; then
-#		echo "Fetching $FILE..."
-#		curl -s --user $USER:$PASS https://$HOST/controller/$ENTITY/$APP_ID/$TYPE -o $APPLICATION/$FILE -k
-#	fi
-#	echo "Converting file $FILE to CSV..."
-#	$SCRIPTPATH/exportCSV.py $ENTITY -i $APPLICATION/$FILE -o $APPLICATION/$ENTITY-$TYPE.csv
+#	if [ -f $APPLICATION/$FILE ]; then
+#		echo "Converting file $FILE to CSV..."
+#		$SCRIPTPATH/exportCSV.py $ENTITY -i $APPLICATION/$FILE -o $APPLICATION/$ENTITY-$TYPE.csv
+#	else
 	echo "Fetch data and translating to CSV..."
 	$SCRIPTPATH/exportCSV.py $ENTITY -s -P 443 -o $APPLICATION/$ENTITY.csv -H ${HOST} -u ${USER} -p ${PASS} -a ${APPLICATION}
+#	fi
 done
 
 ALLOTHERTRAFFIC_LIST=$(curl -s --user $USER:$PASS "https://$HOST/controller/rest/applications/$APP_ID/business-transactions?output=JSON" | grep "_APPDYNAMICS_DEFAULT_TX_" -A2 | grep "id" | awk -F[:,] '{print $2}')
 for BT_ID in ${ALLOTHERTRAFFIC_LIST}; do
 	FILE=allothertraffic-${BT_ID}.json
-#	if [ ! -f $APPLICATION/$FILE ]; then
-#		echo "Fetching $FILE..."
-#		curl -s --user $USER:$PASS "https://$HOST/controller/rest/applications/$APP_ID/request-snapshots/?business-transaction-ids=$BT_ID&time-range-type=BEFORE_NOW&duration-in-mins=1440&output=JSON" -o $APPLICATION/$FILE -k
-#	fi
-#	echo "Converting file $FILE to CSV..."
-#	$SCRIPTPATH/exportCSV.py allothertraffic -i $APPLICATION/$FILE -o $APPLICATION/allothertraffic-${BT_ID}.csv
+#	if [ -f $APPLICATION/$FILE ]; then
+#		echo "Converting file $FILE to CSV..."
+#		$SCRIPTPATH/exportCSV.py allothertraffic -i $APPLICATION/$FILE -o $APPLICATION/allothertraffic-${BT_ID}.csv
+#	else
 	echo "Fetch data and translating to CSV..."
 	$SCRIPTPATH/exportCSV.py allothertraffic -s -P 443 -o $APPLICATION/$FILE.csv -H ${HOST} -u ${USER} -p ${PASS} -a ${APPLICATION}
+#	fi
 done
-
-$SCRIPTPATH/exportCSV.py events -s -P 443 -o $APPLICATION/events.csv -H ${HOST} -u ${USER} -p ${PASS} -a ${APPLICATION}
-#$SCRIPTPATH/exportCSV.py applications -s -P 443 -o applications.csv -H ${HOST} -u ${USER} -p ${PASS}
-#$SCRIPTPATH/exportCSV.py dashboards -s -P 443 -o $APPLICATION/dashboards.csv -H ${HOST} -u ${USER} -p ${PASS} --api-client-name ${APICLIENT} --api-client-secret ${APISECRET}
