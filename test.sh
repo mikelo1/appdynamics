0db265ceee33ba5c9514680300d524632b36c1da
mikelo1 <mosinalde@atsistemas.com>
2020-06-26 16:37:47 +0200

    modules with classes

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 for XML_ENTITY in 	healthrules \
 					transactiondetection-custom; do
 	echo "### Get $XML_ENTITY ###"
-	unzip -p tests.zip $XML_ENTITY.xml | $SCRIPTPATH/exportCSV.py get -f -
+	unzip -p tests.zip $XML_ENTITY.xml | $SCRIPTPATH/appdctl.py get -f -
 #	read -p "Press any key" input
 #	clear
 done
@@ -29,22 +29,7 @@ for JSON_ENTITY in 	actions \
 					request-snapshots \
 					schedules ; do
 	echo "### Get $JSON_ENTITY ###"
-	unzip -p tests.zip $JSON_ENTITY.json | $SCRIPTPATH/exportCSV.py get -f -
+	unzip -p tests.zip $JSON_ENTITY.json | $SCRIPTPATH/appdctl.py get -f -
 #	read -p "Press any key" input
 #	clear
 done
-
-#./appdctl.py get applications
-#./appdctl.py get policies -a hipoteca-portal
-#./appdctl.py get policies -a FullOnline,hipoteca-api-aks,hipoteca-portal
-#./appdctl.py get schedules -a FullOnline,hipoteca-api-aks,hipoteca-portal
-#./appdctl.py get health-rules -a FullOnline,hipoteca-api-aks,hipoteca-portal
-#./appdctl.py get actions -a FullOnline,hipoteca-api-aks,hipoteca-portal
-#./appdctl.py get detection-rules -a FullOnline,hipoteca-api-aks,hipoteca-portal
-#./appdctl.py get businesstransactions -a FullOnline
-#./appdctl.py get backends -a FullOnline
-#./appdctl.py get healthrule-violations -a FullOnline --since=1d12h
-#./appdctl.py get snapshots -a FullOnline --since=1d12h
-#./appdctl.py get allothertraffic -a FullOnline --since=1d12h
-#./appdctl.py patch schedules -a sandbox -p '{"timezone":"Europe\/Belgrade"}'
-#./appdctl.py patch schedules -a sandbox -p '{"timezone":"Europe\/Brussels"}'
