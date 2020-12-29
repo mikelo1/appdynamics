7124183553742de390664e9034e2d592d370ce26
mikelo1 <mosinalde@atsistemas.com>
2020-12-29 14:56:10 +0100

    classes with inheritance

--- policies.py
+++ policies.py
@@ -5,14 +5,65 @@ import sys
 from applications import ApplicationDict
 from appdRESTfulAPI import RESTfulAPI
 
-class PolicyDict:
-    policyDict = dict()
+
+class Entity:
+    entityDict = dict()
 
     def __init__(self):
         pass
 
     def __str__(self):
-        return json.dumps(self.policyDict)
+        return json.dumps(self.entityDict)
+
+    def generate_CSV(self,appID_List,fileName=None):
+        pass
+
+    ###
+     # Generate JSON output from dictionary data
+     # @param appID_List list of application IDs, in order to obtain entities from local entities dictionary
+     # @param fileName output file name
+     # @return None
+    ###
+    def generate_JSON(self,appID_List,fileName=None):
+        if type(appID_List) is not list or len(appID_List)==0: return
+        entityList = [ self.entityDict[str(appID)]  for appID in appID_List ]
+        if fileName is not None:
+            try:
+                with open(fileName, 'w') as outfile:
+                    json.dump(entityList, outfile)
+                outfile.close()
+            except:
+                print ("Could not open output file " + fileName + ".")
+                return (-1)
+        else:
+            print json.dumps(entityList)
+
+    ###
+     # Load entities from a JSON stream data.
+     # @param streamdata the stream data in JSON format
+     # @param appID the ID number of the application where to load the entities data.
+     # @return the number of loaded entities. Zero if no entity was loaded.
+    ###
+    def load(self,streamdata,appID=None):
+        if appID is None: appID = 0
+        try:
+            entities = json.loads(streamdata)
+        except TypeError as error:
+            print ("load_entities: "+str(error))
+            return 0
+        # Add loaded entities to the entities dictionary
+        if type(entities) is dict:
+            self.entityDict.update({str(appID):[entities]})
+        else:
+            self.entityDict.update({str(appID):entities})
+        return len(entities)
+
+
+class PolicyDict(Entity):
+    policyDict = dict()
+
+    def __init__(self):
+        self.policyDict = self.entityDict
 
     def __build_test_policies(self,app_ID):
         policies1=json.loads('[{"id":1854,"name":"POLICY_SANDBOX","enabled":true,"executeActionsInBatch":true,"frequency":null,"actions":[{"actionName":"gogs@acme.com","actionType":"EMAIL","notes":""}],"events":{"healthRuleEvents":null,"otherEvents":[],"anomalyEvents":["ANOMALY_OPEN_CRITICAL"],"customEvents":[]},"selectedEntities":{"selectedEntityType":"ANY_ENTITY"}]')
@@ -169,50 +220,6 @@ class PolicyDict:
                     return (-1)
         if fileName is not None: csvfile.close()
 
-    ###
-     # Generate JSON output from policies data
-     # @param appID_List list of application IDs, in order to obtain policies from local policies dictionary
-     # @param fileName output file name
-     # @return None
-    ###
-    def generate_JSON(self,appID_List,fileName=None):
-        if type(appID_List) is not list or len(appID_List)==0: return
-
-        policies = []
-        for appID in appID_List:
-            policies = policies + self.policyDict[str(appID)]
-
-        if fileName is not None:
-            try:
-                with open(fileName, 'w') as outfile:
-                    json.dump(policies, outfile)
-                outfile.close()
-            except:
-                print ("Could not open output file " + fileName + ".")
-                return (-1)
-        else:
-            print json.dumps(policies)
-
-
-    ###
-     # Load policies from a JSON stream data.
-     # @param streamdata the stream data in JSON format
-     # @param appID the ID number of the application where to load the policies data.
-     # @return the number of loaded policies. Zero if no policy was loaded.
-    ###
-    def load(self,streamdata,appID=None):
-        if appID is None: appID = 0
-        try:
-            policies = json.loads(streamdata)
-        except TypeError as error:
-            print ("load_policies: "+str(error))
-            return 0
-        # Add loaded policies to the policies dictionary
-        if type(policies) is dict:
-            self.policyDict.update({str(appID):[policies]})
-        else:
-            self.policyDict.update({str(appID):policies})
-        return len(policies)
 
     ###
      # Load action details for all actions from an application
